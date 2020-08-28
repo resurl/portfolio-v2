18514c5c106cb0ac7ee2d5e3c83f6ca87a5f3856
resurreccionl <leanna.resurreccion@gmail.com>
2020-08-27 19:40:51 -0700

    added webhooks for netlify

--- cms/api/article/models/article.js
+++ cms/api/article/models/article.js
@@ -1,8 +1,26 @@
 'use strict';
 
+const axios = require('axios');
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    afterUpdate: async entry => {
+        axios
+          .post(strapi.config.currentEnvironment.staticWebsiteBuildURL, {})
+          .catch(() => {
+              // Ignore
+          })
+    },
+
+    afterDestroy: async entry => {
+        axios
+          .post(strapi.config.currentEnvironment.staticWebsiteBuildURL, {})
+          .catch(() => {
+              // Ignore
+          })
+    }
+};
